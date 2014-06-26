b08222755e220dad45d659d9125dfff8dc4f4691
mfdiaspinto <mfdiaspinto@gmail.com>
2014-06-26 11:31:36 +0100

    ...

---
+++ PrimaveraTaskPane-INO/PrimaveraTaskPaneWeb/obj/Debug/Package/PackageTmp/App/Entities/Entities.js
@@ -0,0 +1,84 @@
+﻿
+
+function AppContext() {
+    var name = "";
+    
+    return {
+        getName: function (newName) {
+            return name;
+        },
+        setName: function (newName) {
+            name = newName;
+        }
+    }   
+}
+
+function Formula() {
+    var associativeArray = {};
+    var key = "";
+    var name = "";
+    var formulaName = "";
+    var cell = "A1";
+
+    return {
+        addParameter: function (key, value) {
+            associativeArray[key] = value;
+        },
+        getParameters: function () {
+            return associativeArray;
+        },
+
+        getParameter: function (key) {
+            return associativeArray[key];
+        },
+        getName: function () {
+            return name;
+        },
+        setName: function (newName) {
+            name = newName;
+        },
+        getFormulaName: function () {
+            return formulaName;
+        },
+        setFormulaName: function (newName) {
+            formulaName = newName;
+        },
+        getKey: function () {
+            return key;
+        },
+        setKey: function (newName) {
+            key = newName;
+        },
+        getCell: function () {
+            return cell;
+        },
+        setCell: function (newName) {
+            cell = newName;
+        }
+    }
+}
+
+function ListFormulas() {
+    var list = {};
+    var count = 0;
+    return {
+        add: function (key, value) {
+            if (list[key] == undefined)
+            {
+                list[key] = value;
+                count = count + 1;
+            }
+
+            list[key] = value;
+        },
+        getLists: function () {
+            return list;
+        },
+        getList: function (key) {
+            return list[key];
+        },
+        getCount: function (key) {
+           return count;
+        }
+    }
+}
---
+++ PrimaveraTaskPane-INO/PrimaveraTaskPaneWeb/obj/Debug/Package/PackageTmp/App/Home/data.js
@@ -0,0 +1,112 @@
+﻿
+function loadLists(value){
+	if(value == 'PRIBSS'){
+		 return {
+			    "company": "PRIBSS",
+			   
+			    "lists": [
+			        {
+			            "name": "Sales",
+						"description" : "Sales"
+			        },
+			        {
+			             "name": "Order",
+						 "description" : "Order"
+			        }
+			    ]
+			}
+	}
+	else {
+		return {
+			    "company": "PRITEC",
+			   
+			    "lists": [
+			        {
+			            "name": "Sales",
+						"description" : "Sales Tec"
+			        },
+			        {
+			             "name": "Order",
+						 "description" : "Order Tec"
+			        }
+			    ]
+			}
+	}
+}
+	
+function loadCompanies(){
+  return {
+    "organization": "Primavera",
+   
+    "companies": [
+        {
+            "name": "PRIBSS",
+			"description" : "Primavera Bss"
+        },
+        {
+             "name": "PRITEC",
+			"description" : "Primavera Tec"
+        }
+    ]}
+}
+	
+function loadOrderList(){
+  return {
+	  "data": [
+		   {
+            "key": "ENC.2014.1",
+			"documentType" : "ENC",
+			"serie" : "2014",
+			"number" : "1",
+			"supplier" : "SOFRIO",
+			"total" : 212
+			},
+			 {
+            "key": "ENC.2014.2",
+			"documentType" : "ENC",
+			"serie" : "2014",
+			"number" : "2",
+			"supplier" : "SOFRIO",
+			"total" : 121
+			}, {
+            "key": "ENC.2014.3",
+			"documentType" : "ENC",
+			"serie" : "2014",
+			"number" : "3",
+			"supplier" : "SOFRIO",
+			"total" : 242
+			}
+      ]
+  }
+}
+
+	
+function loadSalesList(){
+  return {
+	  "data": [
+		   {
+            "key": "FA.2014.1",
+			"documentType" : "FA",
+			"serie" : "2014",
+			"number" : "1",
+			"supplier" : "ALCAD",
+			"total" : 212
+			},
+			 {
+            "key": "FA.2014.2",
+			"documentType" : "FA",
+			"serie" : "2014",
+			"number" : "2",
+			"supplier" : "SOFRIO",
+			"total" : 121
+			}, {
+            "key": "FA.2014.3",
+			"documentType" : "FA",
+			"serie" : "2014",
+			"number" : "3",
+			"supplier" : "ALCAD",
+			"total" : 242
+			}
+      ]
+  }
+}
